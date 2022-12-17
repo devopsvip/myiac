d62b643975778079c7ef92bd93941305accfb7cc
zeyangli <lizeyang@zeyangdeMacBook-Pro-2.local>
2022-12-17 13:36:39 +0800

    add jenkins

--- devops/jenkins/pv-pvc.tf
+++ devops/jenkins/pv-pvc.tf
@@ -6,7 +6,8 @@ resource "kubernetes_persistent_volume_v1" "pvjenkins" {
     capacity = {
       storage = "20Gi"
     }
-    access_modes = ["ReadWriteMany"]
+    access_modes       = ["ReadWriteMany"]
+    storage_class_name = "standard"
     persistent_volume_source {
       nfs {
         path   = "/data/storage/kubernetes/apps-jenkins"
@@ -27,7 +28,7 @@ resource "kubernetes_persistent_volume_claim_v1" "pvcjenkins" {
         storage = "20Gi"
       }
     }
-    storage_class_name = ""
-    volume_name = kubernetes_persistent_volume_v1.pvjenkins.metadata.0.name
+    storage_class_name = "standard"
+    volume_name        = kubernetes_persistent_volume_v1.pvjenkins.metadata.0.name
   }
 }
