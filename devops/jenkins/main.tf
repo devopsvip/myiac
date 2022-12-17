9ecfeeaf7d1a3e2ff84c5c5769cabc82fa219938
zeyangli <lizeyang@zeyangdeMacBook-Pro-2.local>
2022-12-17 13:19:03 +0800

    add jenkins

---
+++ devops/jenkins/main.tf
@@ -0,0 +1,89 @@
+terraform {
+  required_providers {
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "2.12.1"
+    }
+  }
+}
+
+provider "kubernetes" {
+  config_path    = "~/.kube/config"
+  insecure       = true
+}
+
+resource "kubernetes_namespace" "jenkins" {
+  metadata {
+    name = "devops"
+  }
+}
+
+resource "kubernetes_deployment_v1" "jenkins" {
+  metadata {
+    name = "jenkins"
+    labels = {
+      app = "jenkins"
+    }
+    namespace = kubernetes_namespace.jenkins.id
+  }
+
+  spec {
+    replicas = 1
+
+    selector {
+      match_labels = {
+        app = "jenkins"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          app = "jenkins"
+        }
+      }
+
+      spec {
+        container {
+          image             = "jenkins/jenkins:2.375.1-jdk11"
+          name              = "jenkins"
+          image_pull_policy = "IfNotPresent"
+
+          port {
+            container_port = 8080
+            name = "web"
+          }
+          port {
+            container_port = 50000
+            name = "agent"
+          }
+
+          resources {
+            limits = {
+              cpu    = 2
+              memory = "4096Mi"
+            }
+            requests = {
+              cpu    = "250m"
+              memory = "1024Mi"
+            }
+          }
+          liveness_probe {
+            http_get {
+              path = "/login"
+              port = 8080
+            }
+              initial_delay_seconds = 30
+              period_seconds        = 3
+          }
+        }
+        volume {
+          persistent_volume_claim {
+            claim_name = kubernetes_persistent_volume_claim_v1.pvcjenkins.name
+          }
+
+        }
+      }
+    }
+  }
+}
---
+++ devops/jenkins/pv-pvc.tf
@@ -0,0 +1,32 @@
+resource "kubernetes_persistent_volume_v1" "pvjenkins" {
+  metadata {
+    name = "pv-jenkins"
+  }
+  spec {
+    capacity = {
+      storage = "20Gi"
+    }
+    access_modes = ["ReadWriteMany"]
+    persistent_volume_source {
+      nfs {
+        path   = "/data/storage/kubernetes/apps-jenkins"
+        server = "192.168.1.200"
+      }
+    }
+  }
+}
+
+resource "kubernetes_persistent_volume_claim_v1" "pvcjenkins" {
+  metadata {
+    name = "pvc-jenkins"
+  }
+  spec {
+    access_modes = ["ReadWriteMany"]
+    resources {
+      requests = {
+        storage = "20Gi"
+      }
+    }
+    volume_name = "${kubernetes_persistent_volume_v1.pvjenkins.metadata.0.name}"
+  }
+}
