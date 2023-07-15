c32c1bcceeec021f2bcd965bf405882e4a219eda
NICE <610556220@qq.com>
2023-07-15 10:07:26 +0800

    Update file run.sh

--- devops/argocd/run.sh
+++ devops/argocd/run.sh
@@ -1,12 +1,12 @@
-# GetDockerImage
-docker pull ccr.ccs.tencentyun.com/argocds/dex:v2.35.3
-docker pull ccr.ccs.tencentyun.com/argocds/redis:7.0.8-alpine
-docker pull ccr.ccs.tencentyun.com/argocds/argocd:v2.6.7
+# # GetDockerImage
+# docker pull ccr.ccs.tencentyun.com/argocds/dex:v2.35.3
+# docker pull ccr.ccs.tencentyun.com/argocds/redis:7.0.8-alpine
+# docker pull ccr.ccs.tencentyun.com/argocds/argocd:v2.6.7
 
-# TagDockerImage
-docker tag ccr.ccs.tencentyun.com/argocds/dex:v2.35.3 ghcr.io/dexidp/dex:v2.35.3 
-docker tag ccr.ccs.tencentyun.com/argocds/argocd:v2.6.7 quay.io/argoproj/argocd:v2.6.7 
-docker tag ccr.ccs.tencentyun.com/argocds/redis:7.0.8-alpine redis:7.0.8-alpine 
+# # TagDockerImage
+# docker tag ccr.ccs.tencentyun.com/argocds/dex:v2.35.3 ghcr.io/dexidp/dex:v2.35.3 
+# docker tag ccr.ccs.tencentyun.com/argocds/argocd:v2.6.7 quay.io/argoproj/argocd:v2.6.7 
+# docker tag ccr.ccs.tencentyun.com/argocds/redis:7.0.8-alpine redis:7.0.8-alpine 
 
 # From local to Kind K8s Cluster
 images=`grep image: install.yaml | awk -F 'image: ' '{print $NF}' | sort -n | uniq`
