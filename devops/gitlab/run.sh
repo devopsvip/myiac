e8ee84e8e739bec0a7a7e98117377ecb811451fd
NICE <610556220@qq.com>
2023-08-03 22:09:52 +0800

    Update 2 files
    - /devops/gitlab/runner-manifests/deployment.yaml
    - /devops/gitlab/run.sh

--- devops/gitlab/run.sh
+++ devops/gitlab/run.sh
@@ -7,11 +7,11 @@ chmod 777 -R /data/storage/kubernetes/gitlab-runner
 
 ## Docker images
 docker pull gitlab/gitlab-ce:16.2.2-ce.0
-docker pull registry.gitlab.com/gitlab-org/gitlab-runner:alpine-v11.6.0
+docker pull gitlab/gitlab-runner:alpine-v16.2.0
 
 ## LoadDockerImage
 kind load  docker-image gitlab/gitlab-ce:16.2.2-ce.0 --name devopscluster
-kind load  docker-image registry.gitlab.com/gitlab-org/gitlab-runner:alpine-v11.6.0 --name devopscluster
+kind load  docker-image gitlab/gitlab-runner:alpine-v16.2.0 --name devopscluster
 
 ## ArgoAPP
 kubectl -n argocd apply -f gitlab-argoapp.yaml 
