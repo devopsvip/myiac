d9651002b32ae63b9754b3cbe6f3dd9422d9e5f0
NICE <610556220@qq.com>
2023-09-01 19:06:13 +0800

    add sonarqube

---
+++ devops/sonarqube/run.sh
@@ -0,0 +1,14 @@
+## NFS
+
+## Gitlab
+mkdir -p /data/storage/kubernetes/sonarqube{conf,logs,data,extensions,lib}
+chmod 777 -R /data/storage/kubernetes/sonarqube
+
+## Docker images
+docker pull sonarqube:9.9.0-community
+
+## LoadDockerImage
+kind load  docker-image sonarqube:9.9.0-community --name devopscluster
+
+## ArgoAPP
+kubectl -n argocd apply -f sonarqube-argoapp.yaml 
