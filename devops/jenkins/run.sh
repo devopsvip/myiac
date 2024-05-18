3820d2e430e9b526c33c4b8ad6a6f6443ba3afb4
Li test <zeyang.li@aliyun.com>
2024-05-18 01:51:58 +0000

    update jenkins run.sh

--- devops/jenkins/run.sh
+++ devops/jenkins/run.sh
@@ -3,13 +3,15 @@
 kind load  docker-image jenkins/jenkins:2.415-jdk11 --name devopscluster
 kind load  docker-image jenkins/inbound-agent:latest --name devopscluster
 
+##jenkins
+mkdir -p /data/storage/kubernetes/jenkins
+chmod -R 777 /data/storage/kubernetes/jenkins
+
 ## ArgoAPP
 kubectl -n argocd apply -f jenkins-argoapp.yaml 
 
 
-##jenkins
-mkdir -p jenkins
-chmod -R 777 /data/storage/kubernetes/jenkins
+
 
 ## Jenkins build cache
 mkdir -p /data/storage/kubernetes/jenkins-build-cache
