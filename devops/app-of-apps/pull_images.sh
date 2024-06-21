00aaa96b2bfc5d55008d533d65542c561bfbda97
Li test <zeyang.li@aliyun.com>
2024-06-21 23:54:13 +0000

    Update 2 files
    - /devops/app-of-apps/pull_images.sh
    - /devops/app-of-apps/load_images.sh

---
+++ devops/app-of-apps/pull_images.sh
@@ -0,0 +1,16 @@
+# GitLab
+docker pull ccr.ccs.tencentyun.com/devopsvip/gitlab-ce:16.2.2-ce.0
+docker pull ccr.ccs.tencentyun.com/devopsvip/gitlab-runner:alpine-v16.2.0
+
+# Jenkins & Agent
+docker pull ccr.ccs.tencentyun.com/devopsvip/jenkins:2.415-jdk11
+docker pull ccr.ccs.tencentyun.com/devopsvip/inbound-agent:latest-jdk11
+
+
+# sonarqube
+docker pull ccr.ccs.tencentyun.com/devopsvip/sonarqube:9.9.0-community
+
+# nexus
+docker pull ccr.ccs.tencentyun.com/devopsvip/nexus3:3.60.0
+# base docker image for ci 
+docker pull ccr.ccs.tencentyun.com/devopsvip/dragonwell:21-anolis
