f4ec7c02c69f692e50b82c5406659bcc67b2c2fb
Li test <zeyang.li@aliyun.com>
2024-07-17 11:51:01 +0000

    Update 2 files
    - /argoapps/logging/init.sh
    - /logging/fluentbit/fluentbit-values.yaml

--- argoapps/logging/init.sh
+++ argoapps/logging/init.sh
@@ -9,3 +9,9 @@ kind load docker-image ccr.ccs.tencentyun.com/devopsvip/fluent-bit:3.1 --name de
 kind load docker-image ccr.ccs.tencentyun.com/devopsvip/kibana:8.5.1 --name devopscluster
 kind load docker-image ccr.ccs.tencentyun.com/devopsvip/elasticsearch:8.5.1 --name devopscluster
 kind load docker-image ccr.ccs.tencentyun.com/devopsvip/busybox:1.30 --name devopscluster
+
+
+
+# after elasticsearch cluster is OK， then set new password
+
+#curl -X POST "https://es.idevops.site/_security/user/elastic/_password" -H "Content-Type: application/json" -k  -u elastic:bJ0tU6YPoKn5GLsJ -d '{"password": "admin123"}'
