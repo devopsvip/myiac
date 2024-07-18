6f457af8880198575a888429b38c7d968e1c792a
Li test <zeyang.li@aliyun.com>
2024-07-18 07:26:48 +0000

    Update file install.sh

--- logging/install.sh
+++ logging/install.sh
@@ -1,12 +1,10 @@
 
 
 
-helm upgrade --install --create-namespace -n logging elasticsearch ./elasticsearch -f values/es-values.yaml
+helm upgrade --install --create-namespace -n logging elasticsearch ./elasticsearch -f elasticsearch/values.yaml  -f elasticsearch/es-values.yaml
 
 kubectl get secrets --namespace=logging elasticsearch-master-credentials -ojsonpath='{.data.password}' | base64 -d
 
+helm upgrade --install -n logging kibana ./kibana -f kibana/values.yaml -f kibana/kibana-values.yaml
 
-helm upgrade --install -n logging kibana ./kibana -f values/kibana-values.yaml
-
-helm upgrade --install fluentbit ./fluent-bit -n logging -f values/fluent-bit-values.yaml  
-
+helm upgrade --install fluentbit ./fluentbit -n logging -f fluentbit/values.yaml -f fluentbit/fluentbit-values.yaml
